c118b4e5ac55b44221f5669cb3f9eb53fbb540d0
The Exorcist <whitebeard086@gmail.com>
2022-09-20 22:50:46 +0100

    Validate Inputs

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,6 +26,12 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required',
+        ]);
+        
         return Product::create($request->all());
     }
 
